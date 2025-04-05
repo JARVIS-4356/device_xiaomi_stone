47523a7ad3c57c9997ce9842c551cf3f9b5bba4d
JARVIS <41569276+JARVIS-4356@users.noreply.github.com>
2025-04-05 16:40:51 +0000

    sm6375-common: audio: Add support for Dolby Atmos

--- device.mk
+++ device.mk
@@ -4,8 +4,8 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Call the ViperFX Config
-$(call inherit-product-if-exists, packages/apps/ViPER4AndroidFX/config.mk)
+# Call the Dolby Atmos Config
+$(call inherit-product, vendor/dolby/dolby.mk)
 
 # Enable project quotas and casefolding for emulated storage without sdcardfs
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
