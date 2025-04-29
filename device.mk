0e7f7413ab03c2487beacc8c8587bd01af3ae0f1
JARVIS <41569276+JARVIS-4356@users.noreply.github.com>
2025-04-29 07:35:48 +0000

    stone: Add support for Dolby Vision

--- device.mk
+++ device.mk
@@ -7,6 +7,9 @@
 # Call the Dolby Atmos Config
 $(call inherit-product, vendor/sony/dolby/dolby.mk)
 
+# Call the Dolby Vision Config
+$(call inherit-product, hardware/dolby-vision/dolby-vision.mk)
+
 # Enable project quotas and casefolding for emulated storage without sdcardfs
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
